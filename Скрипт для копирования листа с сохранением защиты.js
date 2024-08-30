084584fe9bd79e8232ffa497739b30db5173fd6f
FoKiRu <xkiladx@gmail.com>
2024-08-30 17:47:32 +0300

    Создание копии с редакторами листа

--- Скрипт для копирования листа с сохранением защиты.js
+++ Скрипт для копирования листа с сохранением защиты.js
@@ -9,11 +9,35 @@ function duplicateSheetWithProtection() {
     ss.setActiveSheet(newSheet);  // Делаем новый лист активным
     ss.moveActiveSheet(2);  // Перемещаем его на вторую позицию (индекс 2)
 
-    // Копирование защищённых диапазонов
-    var protections = sheet.getProtections(SpreadsheetApp.ProtectionType.RANGE);
-    for (var i = 0; i < protections.length; i++) {
-        var protection = protections[i].copy();
-        protection.setRange(newSheet.getRange(protection.getRange().getA1Notation()));
+    // Копирование защиты листа (если есть)
+    var sheetProtections = sheet.getProtections(SpreadsheetApp.ProtectionType.SHEET);
+    if (sheetProtections.length > 0) {
+        var sheetProtection = sheetProtection[0]; // Защита листа (ожидается одна)
+
+        // Создание новой защиты на новом листе
+        var newSheetProtection = newSheet.protect();
+        newSheetProtection.setDescription(sheetProtection.getDescription());
+        newSheetProtection.setWarningOnly(sheetProtection.isWarningOnly());
+
+        // Копирование исключений (диапазонов с исключениями)
+        var unprotectedRanges = sheetProtection.getUnprotectedRanges();
+        var newUnprotectedRanges = [];
+        for (var j = 0; j < unprotectedRanges.length; j++) {
+            var range = unprotectedRanges[j];
+            var newRange = newSheet.getRange(range.getA1Notation());
+            newUnprotectedRanges.push(newRange);
+        }
+        newSheetProtection.setUnprotectedRanges(newUnprotectedRanges);
+
+        // Копирование редакторов
+        if (!sheetProtection.isWarningOnly()) {
+            newSheetProtection.removeEditors(newSheetProtection.getEditors());
+            newSheetProtection.addEditors(sheetProtection.getEditors());
+
+            if (sheetProtection.canDomainEdit()) {
+                newSheetProtection.setDomainEdit(true);
+            }
+        }
     }
     
     // Устанавливаем оригинальный лист активным, чтобы вернуть пользователя на исходную позицию
